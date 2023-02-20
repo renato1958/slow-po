f4452b84a4f8a6968ca22965e6074ca641234f4d
renato1958 <felisi.renato@gmail.com>
2023-02-20 01:15:59 -0500

    Risolti alcuni problemi di layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="main-container">
         <header class="page-header">
-            <h1 class="primary-title">Slow Po </h1><span class="secondary-title">Il portale del lungoticino</span>
+            <h1 class="primary-title">Slow Po </h1>
+            <span class="secondary-title">Il portale del lungoticino</span>
         </header>
         <main class="main-content">
             <section class="image">
@@ -18,40 +19,49 @@
 <style scoped>
 .main-container {
     min-height: 100vh;
-    margin: 0 auto;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: stretch;
+    align-items: center;
+}
+
+.main-content {
+    flex: 1 1;
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-evenly;
     align-items: center;
 }
 
 .page-header {
-    height: 150px;
+    height: 120px;
     display: flex;
     width: 100%;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     background-color: var(--straw-color);
 }
 
 .primary-title {
-    font-size: 3rem;
+    font-size: 2rem;
     color: #345678;
     font-weight: 800;
     text-shadow: 3px 1px 1px grey;
-    margin-right: 10%
+    margin-left: 7%;
 }
 
 .secondary-title {
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     font-style: italic;
     color: #676767;
     font-weight: 700;
+    margin-right: 7%;
 }
 
 .image {
-    width: 60%;
-    margin: 100px auto;
+    width: 75%;
+    max-width: 960px;
 }
 
 .image img {
@@ -60,7 +70,6 @@
 }
 
 .message {
-    width: 100%;
     text-align: center;
 }
 
